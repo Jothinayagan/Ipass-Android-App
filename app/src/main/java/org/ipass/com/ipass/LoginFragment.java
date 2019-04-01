2df5cc0a7730e7796769649540850e4a38df8dba
Gowtham C <gowtham366.vpi@gmail.com>
2019-04-01 23:30:14 +0530

    Update LoginFragment.java

--- app/src/main/java/org/ipass/com/ipass/LoginFragment.java
+++ app/src/main/java/org/ipass/com/ipass/LoginFragment.java
@@ -29,7 +29,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Created by Abishek on 11-Apr-18.
+ * Created by Gowtham on 11-Apr-18.
  */
 
 public class LoginFragment extends Fragment{
